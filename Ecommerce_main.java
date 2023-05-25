36818264127408617687f4a4b142c821ddf61b28
sujay <123sujaygk@gmail.com>
2023-05-25 17:36:31 +0530

    First commit

---
+++ Ecommerce_main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oops_java;
+
+/**
+ *
+ * @author user
+ */
+public class Ecommerce_main {
+    
+    public static void main(String[] args){
+        
+        commerce_login ccl = new commerce_login();
+        ccl.show();
+    }
+}
